5110182f588ea1aef6aa332380787ac15e14034f
AlessioRosatelli <alessio.rosatelli1994@gmail.com>
2017-09-14 16:22:13 +0200

    Test classe principale
    E' stata creata una classe di test ( di tipo Junit Case) per la classe principale Ristorante e ne sono stati testati i metodi più significativi

--- Ristorante/src/test/ObsOrdinazioneTest.java
+++ Ristorante/src/test/ObsOrdinazioneTest.java
@@ -1,13 +1,25 @@
 package test;
 
+import controller.Contatore;
 import controller.IObsOrd;
 import model.Ordinazione;
 
 public class ObsOrdinazioneTest implements IObsOrd {
+	
+	private Contatore c= new Contatore();
 
 	@Override
 	public void update(Ordinazione ord) {
+		c.incrementa(1);
 
+	}
+
+	public Contatore getC() {
+		return c;
+	}
+
+	public void setC(Contatore c) {
+		this.c = c;
 	}
 
 }
--- Ristorante/src/test/ObsTavoloTest.java
+++ Ristorante/src/test/ObsTavoloTest.java
@@ -1,12 +1,24 @@
 package test;
 
+import controller.Contatore;
 import controller.IObsTavolo;
 import model.TavoloAbs;
 
 public class ObsTavoloTest implements IObsTavolo {
+	
+	private Contatore c = new Contatore ();
 
 	@Override
 	public void update(TavoloAbs t) {
+		c.incrementa(1);
+	}
+
+	public Contatore getC() {
+		return c;
+	}
+
+	public void setC(Contatore c) {
+		this.c = c;
 	}
 
 }
